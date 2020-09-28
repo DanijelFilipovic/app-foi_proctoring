f0c5fd2c06004e58168d1ff6195ef7276f68d5a3
DanijelFilipovic <danijel.filipovic@outlook.com>
2020-09-28 20:55:54 +0200

    Created and implemented RecordingController.

---
+++ src/Controller/RecordingController.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Model\Table\UserSessionsTable;
+
+class RecordingController extends AppController {
+	
+	private UserSessionsTable $userSessionsTable;
+	
+	public function initialize(): void {
+		parent::initialize();
+		$this->userSessionsTable = $this->getTableLocator()->get('UserSessions');
+	}
+	
+	public function start() {
+		$message = 'Could not update the UserSessions table with the start of recording.';
+		$userSession = $this->getUserSession();
+		if ($userSession != null) {
+			$userSession->recording = 1;
+			$this->userSessionsTable->save($userSession);
+			$message = 'Recording marked as started in UserSessions table.';
+		}
+		$this->set('message', $message);
+	}
+	
+	public function stop() {
+		$message = 'Could not update the UserSessions table with the stop of recording.';
+		$userSession = $this->getUserSession();
+		if ($userSession != null) {
+			$userSession->recording = 0;
+			$this->userSessionsTable->save($userSession);
+			$message = 'Recording marked as stopped in UserSessions table.';
+		}
+		$this->set('message', $message);
+	}
+	
+	private function getUserSession() {
+		$session = $this->request->getSession();
+		return $this->userSessionsTable
+			->find()
+			->where(['session_id' => $session->id()])
+			->first();
+	}
+
+}
