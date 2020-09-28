d1a5c79d5046616d5f961b3fa1df295e4a154d8f
Danijel <Danijel@Jagodica>
2020-09-28 20:53:55 +0200

    Added record ad stop icons on buttons.

--- templates/Pages/index.php
+++ templates/Pages/index.php
@@ -4,8 +4,8 @@ $this->Html->script('fpp', ['block' => true]);
 ?>
 
 <div class="recording-buttons">
-	<button id="record-button">Record</button>
-	<button id="stop-button">Stop</button>
+	<button id="record-button">&#x23FA;</button>
+	<button id="stop-button" disabled>&#x23F9;</button>
 </div>
 <div class="recording-video">
 	<video id="video" width="800" height="600" autoplay></video>
